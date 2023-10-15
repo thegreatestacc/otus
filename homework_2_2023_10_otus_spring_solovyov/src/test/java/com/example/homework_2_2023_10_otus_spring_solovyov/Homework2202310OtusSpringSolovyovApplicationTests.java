265509feabeda188c8ec67124d9f36e9ae2e7fbc
nobody <thegreatestemailname@gmail.com>
2023-10-15 14:24:21 +0300

    added module for second hw

---
+++ homework_2_2023_10_otus_spring_solovyov/src/test/java/com/example/homework_2_2023_10_otus_spring_solovyov/Homework2202310OtusSpringSolovyovApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homework_2_2023_10_otus_spring_solovyov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework2202310OtusSpringSolovyovApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
