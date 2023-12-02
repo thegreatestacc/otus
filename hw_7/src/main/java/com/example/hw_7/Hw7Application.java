482f4b34af4304121d0d34a3084a053d96d3512d
nobody <thegreatestemailname@gmail.com>
2023-12-02 17:17:13 +0300

    hw_7 template

---
+++ hw_7/src/main/java/com/example/hw_7/Hw7Application.java
@@ -0,0 +1,13 @@
+package com.example.hw_7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw7Application.class, args);
+    }
+
+}
