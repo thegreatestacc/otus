22135e89311355b7dbcbcf50af9996f597126e81
nobody <thegreatestemailname@gmail.com>
2023-12-08 21:55:33 +0300

    wip

--- hw_3/src/test/java/com/example/hw_3/dao/CsvQuestionDaoTest.java
+++ hw_3/src/test/java/com/example/hw_3/dao/CsvQuestionDaoTest.java
@@ -11,7 +11,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
-@TestPropertySource("classpath:")
+@TestPropertySource("classpath:application-test.yml")
 class CsvQuestionDaoTest {
 
     @Autowired
