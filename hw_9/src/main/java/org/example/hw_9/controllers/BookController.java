0b7d6457f8f3f6908167d15cd6edb78d7ffc6f85
nobody <thegreatestemailname@gmail.com>
2024-01-09 20:13:14 +0300

    wip-3 added views

--- hw_9/src/main/java/org/example/hw_9/controllers/BookController.java
+++ hw_9/src/main/java/org/example/hw_9/controllers/BookController.java
@@ -1,11 +1,14 @@
 package org.example.hw_9.controllers;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.example.hw_9.models.Book;
 import org.example.hw_9.services.BookService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -19,6 +22,16 @@ public class BookController {
     public String showAllBooks(Model model) {
         List<Book> books = bookService.findAll();
         model.addAttribute("books", books);
-        return "index";
+        return "books";
+    }
+
+    @PostMapping("/add")
+    public String addBook(@Valid Book book, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            model.addAttribute(book);
+            return "add-book";
+        }
+        bookService.insert(book.getTitle(), book.getAuthor().getId(), book.getGenre().getId());
+        return "redirect:/books";
     }
 }
