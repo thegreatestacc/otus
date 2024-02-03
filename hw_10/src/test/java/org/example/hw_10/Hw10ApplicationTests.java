26d214354137975f32d7892ad65e1decdfb26aa9
nobody <thegreatestemailname@gmail.com>
2024-02-03 12:19:28 +0300

    hw_10 init commit

---
+++ hw_10/src/test/java/org/example/hw_10/Hw10ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.hw_10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw10ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
