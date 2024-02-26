e5ae3cbe826bc661d2561940e4568c008cdfcc47
nobody <thegreatestemailname@gmail.com>
2024-02-26 21:48:12 +0300

    hw_11 wip-1 added builder

--- hw_11/src/main/java/org/example/hw_11/dto/book/BookCreateDto.java
+++ hw_11/src/main/java/org/example/hw_11/dto/book/BookCreateDto.java
@@ -2,16 +2,14 @@ package org.example.hw_11.dto.book;
 
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import lombok.experimental.FieldDefaults;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
+@Builder
 public class BookCreateDto {
     @NotNull
     Long bookId;
